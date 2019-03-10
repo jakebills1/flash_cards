5b1df67408d5ac681494f1315c4a9dfa24ca315d
Jake Bills <jakebills1@gmail.com>
2019-03-10 13:07:23 -0600

    add and delete options for cards working

--- script.js
+++ script.js
@@ -11,15 +11,20 @@ $(document).ready( function(){
     }
 
   })
+  $(document).on("click", "#delete-card", function(){
+    let card = this.parentElement.parentElement;
+    card.remove();
+  })
   $("#submit").click( function(){
     let values = document.querySelectorAll("input");
-    let newCard = `<div class="card">
+    let newCard = `<div class="card small">
         <div class="card-content">
           <p class="question"><i class="fas fa-question fa-2x"></i>${values[0].value}</p>
           <p class="answer hide">A: ${values[1].value}</p>
         </div>
         <div class="card-action">
           <a href="#" id="flip-card">Flip card</a>
+          <a href="#" id="delete-card">Delete Card</a>
         </div>
       </div>`
     let cardDiv = document.querySelector(".container");
